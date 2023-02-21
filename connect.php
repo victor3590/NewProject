838b6fceb36d64a0bb8ee5625f3e6ad6f6827119
victor3590 <124095440+victor3590@users.noreply.github.com>
2023-02-21 17:14:09 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,15 +5,20 @@
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 	$number = $_POST['number'];
+	
+	$servername = "phpMyAdmin";
+	$database = "UserName";
+	$username = "username";
+	$password = "password";
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli($servername, $username, $password, $database);
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$stmt = $conn->prepare("insert into registration(Name, LastName, Id, password) values(?, ?, ?, ?)");
+		$stmt->bind_param("sssi", $firstName, $lastName,, $Id, $password);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
