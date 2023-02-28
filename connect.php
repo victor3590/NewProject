47797c603eaa9667c38a8af6420cd1c32408d87b
victor3590 <124095440+victor3590@users.noreply.github.com>
2023-02-28 15:48:24 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@
 	$password = $_POST['password'];
 	$number = $_POST['number'];
 	
-	$servername = 'phpMyAdmin';
+	$servername = 'localhost';
 	$database = 'lliguiv_new data';
 	$username = 'lliguiv1';
 	$password = 'Newmontclair1';
